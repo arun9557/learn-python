faa83b0ea6faca754b4f23a52f1de06b79f2f7f5
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-08-15 19:15:51 +0530

    here

--- hellow.py
+++ hellow.py
@@ -12,9 +12,16 @@
 print (2+4)
 print (8+4)
 print (8-4)
+print(2**3) # output = 8 so its int
+print(2//4) # output = 0 so its int
 # how to check data type
 print(type(2+4))
 # 2----------- float
 print(2/3) 
 # output = 0.6666666666666666 so its float
 print(type(2/3))
+print(64%7)
+
+
+
+
