a52889fa5f45c34ddb9e381fba7b88af706e65ca
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-08-15 20:06:43 +0530

    here

--- hellow.py
+++ hellow.py
@@ -8,6 +8,9 @@
 # set
 # tuple
 
+# print(): Yeh screen pe kuch show karta hai.
+# type(): Yeh batata hai ki data ka type kya hai (int, float, ya kuch aur).
+
 # 1----------int 
 print (2+4)
 print (8+4)
@@ -21,7 +24,12 @@ print(2/3)
 # output = 0.6666666666666666 so its float
 print(type(2/3))
 print(64%7)
+# math function 
+    #   a. round(): Yeh kisi number ko round karta hai.
+    print(round(3.1))  # Output: 3
+    print(round(3.9))  # Output: 4
 
-
-
+    print(round(2/3, 2)) # output = 0.67 so its float
+        #  b. abs(): Yeh kisi number ka absolute value return karta hai.
+    print(ads(-3))  # Output: 3
 
