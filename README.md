8bf6fe6941e7481dc3ad9e23e724f72664f39345
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-08-15 19:06:35 +0530

    Add hellow.py and update README.md

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Welcome to GitHub Desktop!
-
-This is your README. READMEs are where you can communicate what your project is and how to use it.
-
-Write your name on line 6, save it, and then head back to GitHub Desktop.
+# Data types in Python:
+int 
+float
+bool
+str
+list
+dict    
+set
+tuple
---
+++ hellow.py
@@ -0,0 +1,20 @@
+# Data types in Python:
+# int 
+# float
+# bool
+# str
+# list
+# dict    
+# set
+# tuple
+
+# 1----------int 
+print (2+4)
+print (8+4)
+print (8-4)
+# how to check data type
+print(type(2+4))
+# 2----------- float
+print(2/3) 
+# output = 0.6666666666666666 so its float
+print(type(2/3))
