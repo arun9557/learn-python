4ad9a7b2db4cbad23091aca7a202457bb15b1893
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-09-08 23:01:10 +0530

    Update Variables4.py

--- Variables4.py
+++ Variables4.py
@@ -17,3 +17,5 @@ final_value = value1 + 5
 print(final_value)  # Output: 15
 # Here, value1 is assigned 10, and final_value is calculated as value1 + 5, resulting in 15.
 print(type(final_value))
+
+yaha se start karna hia yrr 
