8169b79d8ed2879f211a1fe8adea80dc05d35c46
A₹UN $HEKHAR <117110654+arun9557@users.noreply.github.com>
2025-09-25 23:08:44 +0530

    ln

---
+++ day1.py
@@ -0,0 +1,22 @@
+# expressions vs statements
+iq = 100
+user_age = iq / 5 # expression
+print(user_age) # statement
+
+#-------------------------------------------------------------------
+# argumented assigment operator
+
+some_value = 5
+some_value = some_value + 2 #(some_value -= 2 is also valid)
+print(some_value) # output 7
+
+#-------------------------------------------------------------------
+# Matrix
+matrix = [[
+    [1, 2, 3],
+    [4, 5, 6],   #these type of data structure is called 2D array
+    [7, 8, 9]     # 
+]]  
+
+print(matrix[0][1])
+
